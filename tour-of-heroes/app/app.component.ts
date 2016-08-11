430a208571e32de7a3b5540ad180d806f18b16d2
rcornacchia <rlc2160@columbia.edu>
2016-08-11 16:51:34 -0400

    redoing quickstart to regain feel

--- tour-of-heroes/app/app.component.ts
+++ tour-of-heroes/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail.component';
 
